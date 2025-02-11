d9a2538df7a18a04dda05d3b40b2af217b9c5481
Jackson Lins <jlinssilva@gmail.com>
2025-02-11 17:46:28 -0300

    Reestruturação HTML + JS (Header e Footer inclusos no HTML dinamicamente via JS)

--- jscript/faleconosco.js
+++ jscript/faleconosco.js
@@ -2,6 +2,32 @@ let inputContato = document.getElementById('nomecontato');
 let usernameLabel = document.querySelector('label[for="nomecontato"]');
 let usernameHelper = document.getElementById("nomecontato-helper");
 
+let inputEmail = document.getElementById('email');
+let emailLabel = document.querySelector('label[for="emailcontato"]');
+let emailHelper = document.getElementById("emailcontato-helper");
+
+let inputWhatsapp = document.getElementById('whatsapp');
+
+let inputMensagem = document.getElementById('mensagem');
+
+function validarFormulario(){
+    let nomeContato = inputContato.value;
+    let emailContato = inputEmail.value;
+    let mensagemContato = inputMensagem.value;
+
+    if(nomeContato.length < 3){
+        alert("O campo nome precisa ter no mínimo 3 caracteres.");
+    }
+
+    if(emailContato.length < 3){
+        alert("O campo email precisa ter no mínimo 3 caracteres.");
+    }
+
+    if(mensagemContato.length < 3){
+        alert("O campo mensagem precisa ter no mínimo 3 caracteres.");
+    }
+}
+
 
 function validarInput(input, min, helper){
     input.addEventListener("blur", function(elemento){
@@ -15,10 +41,5 @@ function validarInput(input, min, helper){
 validarInput(inputContato, 3);
 
 
-//btnEnviar.addEventListener('click', validarFormulario);
-/*
-inputNome.addEventListener('change', function(elemento){
-    let valorInput = elemento.target.value;
-    console.log(valorInput);
-});
-*/
+btnEnviar.addEventListener('click', validarFormulario);
+
